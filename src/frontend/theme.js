836f09554e957eac7c35d8bf0bb634966c779954
Lucho Rabu <lucianorabu@gmail.com>
2024-02-20 19:46:50 +0000

    better shadow

--- src/frontend/theme.js
+++ src/frontend/theme.js
@@ -25,7 +25,7 @@ const theme = {
     ipad: '(min-width: 1024px) and (min-height: 1200px)',
     desktop: '(min-width: 1024px)',
   },
-  shadow: '0px 1px 4px rgba(0, 0, 0, 25%)',
+  shadow: '0px 0px 20px rgba(0,0,0,40%)',
   zIndexes: {
     navShadow: 9,
     nav: 10,
